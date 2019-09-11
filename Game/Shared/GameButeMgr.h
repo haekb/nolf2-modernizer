23d59689ebf131ce4256bb63b5d75875b5522449
Jake <haekbreen@gmail.com>
2019-09-10 22:28:48 -0700

    Centralize code.

--- Game/Shared/GameButeMgr.h
+++ Game/Shared/GameButeMgr.h
@@ -25,42 +25,7 @@ void GBM_DisplayError(const char* szMsg);
 // Character pointer hash map definition...
 //
 // ----------------------------------------------------------------------- //
-
-class GameButeMgr_Hasher {
-public:
-	enum { bucket_size = 10 };
-
-	GameButeMgr_Hasher() {}
-
-	size_t operator()(const char* key) const {
-		return hash(key);
-	}
-
-	bool operator()(const char* left, const char* right) const {
-		return compare(left, right);
-	}
-private:
-	// Was `equal_str_nocase`, need to left side.
-	bool compare(const char* s1, const char* s2) const
-	{
-		return stricmp(s1, s2) < 0;
-	}
-	// Was hash_str_nocase, still kinda is!
-	// Copied for stl-port's std::hash<const char*>.
-	// Added tolower function on the string.
-	unsigned long hash(const char* str) const
-	{
-		unsigned long hash = 0;
-		for (; *str; ++str)
-			hash = 5 * hash + tolower(*str);
-
-		return hash;
-	}
-};
-
-typedef stdext::hash_map<const char *,int, GameButeMgr_Hasher> IndexTable;
-
-
+typedef stdext::hash_map<const char *,int, ButeMgr_Hasher> IndexTable;
 
 class CGameButeMgr
 {
