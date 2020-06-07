d3f7624892aec020c14ac4da29eb5f48af32f36e
Jake <haekbreen@gmail.com>
2020-06-06 19:01:57 -0700

    Remove unused discord config info

--- config/discord-secret-example.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// Add your ids here, don't include in git!
-#define DISCORD_CLIENT_ID 
-#define DISCORD_SECRET_ID 
--- config/discord-secret.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-// We only need client id
-#define DISCORD_CLIENT_ID 634215704022745098
