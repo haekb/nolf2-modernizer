a7f2244f607e16bc9f4605c4cb54166f4d5e9d49
Jake <haekbreen@gmail.com>
2020-08-05 13:14:15 -0700

    Finish off our definitions, and bindings.

--- Game/ClientShellDLL/ClientShellShared/GameInputMgr.cpp
+++ Game/ClientShellDLL/ClientShellShared/GameInputMgr.cpp
@@ -6,6 +6,8 @@
 	
 GameInputMgr* g_pGameInputMgr = nullptr;
 
+#define BIND_FUNC(x) m_pInputMgr->x = GameInputMgr::x
+
 GameInputMgr::GameInputMgr()
 {
 	g_pGameInputMgr = this;
@@ -37,12 +39,34 @@ GameInputMgr::~GameInputMgr()
 	// Engine will clean up
 }
 
+
 void GameInputMgr::ReplaceBindings()
 {
-	m_pInputMgr->Init = GameInputMgr::Init;
-	m_pInputMgr->Term = GameInputMgr::Term;
-	m_pInputMgr->IsInitted = GameInputMgr::IsInitted;
-	m_pInputMgr->ReadInput = GameInputMgr::ReadInput;
+	BIND_FUNC(Init);
+	BIND_FUNC(Term);
+	BIND_FUNC(IsInitted);
+	BIND_FUNC(ListDevices);
+	BIND_FUNC(PlayJoystickEffect);
+	BIND_FUNC(ReadInput);
+	BIND_FUNC(FlushInputBuffers);
+	BIND_FUNC(ClearInput);
+	BIND_FUNC(AddAction);
+	BIND_FUNC(EnableDevice);
+	BIND_FUNC(ClearBindings);
+	BIND_FUNC(AddBinding);
+	BIND_FUNC(ScaleTrigger);
+	BIND_FUNC(GetDeviceBindings);
+	BIND_FUNC(FreeDeviceBindings);
+	BIND_FUNC(StartDeviceTrack);
+	BIND_FUNC(TrackDevice);
+	BIND_FUNC(EndDeviceTrack);
+	BIND_FUNC(GetDeviceObjects);
+	BIND_FUNC(FreeDeviceObjects);
+	BIND_FUNC(GetDeviceName);
+	BIND_FUNC(GetDeviceObjectName);
+	BIND_FUNC(IsDeviceEnabled);
+	BIND_FUNC(ShowDeviceObjects);
+	BIND_FUNC(ShowInputDevices);
 }
 
 bool GameInputMgr::Init(InputMgr* pInputMgr, intptr_t* pState)
@@ -65,9 +89,110 @@ bool GameInputMgr::IsInitted(InputMgr* pInputMgr)
 	return false;
 }
 
+void GameInputMgr::ListDevices(InputMgr* pInputMgr)
+{
+}
+
+uint32_t GameInputMgr::PlayJoystickEffect(InputMgr* pInputMgr, const char* szEffectName, float x, float y)
+{
+	return 0;
+}
+
 void GameInputMgr::ReadInput(InputMgr* pInputMgr, uint8_t* pActionsOn, float fAxisOffsets[3])
 {
 
 	bool bBeans = true;
 
+}
+
+bool GameInputMgr::FlushInputBuffers(InputMgr* pInputMgr)
+{
+	return false;
+}
+
+LTRESULT GameInputMgr::ClearInput()
+{
+	return LTRESULT();
+}
+
+void GameInputMgr::AddAction(InputMgr* pInputMgr, const char* pActionName, int nActionCode)
+{
+}
+
+bool GameInputMgr::EnableDevice(InputMgr* pInputMgr, const char* pDeviceName)
+{
+	return false;
+}
+
+bool GameInputMgr::ClearBindings(InputMgr* pInputMgr, const char* pDeviceName, const char* pTriggerName)
+{
+	return false;
+}
+
+bool GameInputMgr::AddBinding(InputMgr* pInputMgr, const char* pDeviceName, const char* pTriggerName, const char* pActionName, float fRangeLow, float fRangeHigh)
+{
+	return false;
+}
+
+bool GameInputMgr::ScaleTrigger(InputMgr* pInputMgr, const char* pDeviceName, const char* pTriggerName, float fScale, float fRangeScaleMin, float fRangeScaleMax, float fRangeScalePreCenterOffset)
+{
+	return false;
+}
+
+DeviceBinding* GameInputMgr::GetDeviceBindings(uint32_t nDevice)
+{
+	return nullptr;
+}
+
+void GameInputMgr::FreeDeviceBindings(DeviceBinding* pBindings)
+{
+}
+
+bool GameInputMgr::StartDeviceTrack(InputMgr* pMgr, uint32_t nDevices, uint32_t nBufferSize)
+{
+	return false;
+}
+
+bool GameInputMgr::TrackDevice(DeviceInput* pInputAttay, uint32_t* pInOut)
+{
+	return false;
+}
+
+bool GameInputMgr::EndDeviceTrack()
+{
+	return false;
+}
+
+DeviceObject* GameInputMgr::GetDeviceObjects(uint32_t nDeviceFlags)
+{
+	return nullptr;
+}
+
+void GameInputMgr::FreeDeviceObjects(DeviceObject* pList)
+{
+}
+
+bool GameInputMgr::GetDeviceName(uint32_t nDeviceType, char* szBuffer, uint32_t nBufferSize)
+{
+	return false;
+}
+
+bool GameInputMgr::GetDeviceObjectName(const char* szDeviceName, uint32_t nDeviceObjectID, char* szDeviceObjectName, uint32_t nDeviceObjectNameLength)
+{
+	return false;
+}
+
+bool GameInputMgr::IsDeviceEnabled(const char* szDeviceName)
+{
+	return false;
+}
+
+bool GameInputMgr::ShowDeviceObjects(const char* szDeviceName)
+{
+	return false;
+}
+
+bool GameInputMgr::ShowInputDevices()
+{
+	return false;
 }
--- Game/ClientShellDLL/ClientShellShared/GameInputMgr.h
+++ Game/ClientShellDLL/ClientShellShared/GameInputMgr.h
@@ -24,12 +24,66 @@ public:
 	static void Term(InputMgr* pInputMgr);
 	static bool IsInitted(InputMgr* pInputMgr);
 
+	static void ListDevices(InputMgr* pInputMgr);
+
+	static uint32_t PlayJoystickEffect(InputMgr* pInputMgr, const char* szEffectName, float x, float y);
+
 	static void ReadInput(InputMgr* pInputMgr, uint8_t* pActionsOn, float fAxisOffsets[3]);
 
+	static bool FlushInputBuffers(InputMgr* pInputMgr);
+
+	static LTRESULT ClearInput();
+
+	static void AddAction(InputMgr* pInputMgr, const char* pActionName, int nActionCode);
+	
+
+	static bool EnableDevice(InputMgr* pInputMgr, const char* pDeviceName);
+	
+
+	static bool ClearBindings(InputMgr* pInputMgr, const char* pDeviceName, const char* pTriggerName);
+	
+
+	static bool AddBinding(InputMgr* pInputMgr, const char* pDeviceName, const char* pTriggerName, const char* pActionName, float fRangeLow, float fRangeHigh);
+	
+
+	static bool ScaleTrigger(InputMgr* pInputMgr, const char* pDeviceName, const char* pTriggerName, float fScale, float fRangeScaleMin, float fRangeScaleMax, float fRangeScalePreCenterOffset);
+	
+
+	static DeviceBinding* GetDeviceBindings(uint32_t nDevice);
+	
+
+	static void FreeDeviceBindings(DeviceBinding* pBindings);
+	
+
+	static bool StartDeviceTrack(InputMgr* pMgr, uint32_t nDevices, uint32_t nBufferSize);
+	
+
+	static bool TrackDevice(DeviceInput* pInputAttay, uint32_t* pInOut);
+	
+
+	static bool EndDeviceTrack();
+	
+
+	static DeviceObject* GetDeviceObjects(uint32_t nDeviceFlags);
+	
+
+	static void FreeDeviceObjects(DeviceObject* pList);
+	
+
+	static bool GetDeviceName(uint32_t nDeviceType, char* szBuffer, uint32_t nBufferSize);
+	
+
+	static bool GetDeviceObjectName(const char* szDeviceName, uint32_t nDeviceObjectID, char* szDeviceObjectName, uint32_t nDeviceObjectNameLength);
+	
+
+	static bool IsDeviceEnabled(const char* szDeviceName);
+
+	static bool ShowDeviceObjects(const char* szDeviceName);
+
+	static bool ShowInputDevices();
 
 	// Public so static functions can clean them up.
 	InputMgr* m_pInputMgr;
-	InputMgr* m_pOldInputMgr;
 
 private:
 
